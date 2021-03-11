a599555a8aca83c2f1eeb82b9db0f59991db2635
FerasTr <32588570+FerasTr@users.noreply.github.com>
2021-03-11 19:48:24 +0200

    Add files via upload

---
+++ date.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+date=$(date '+%a %e %b %k:%M')
+echo "%{T1} $date%{T-}"
---
+++ rofi-powermenu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+if [ -z "$@" ]; then
+    echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
+    echo -en "Logout\0icon\x1fsystem-log-out\n"
+    echo -en "Suspend\0icon\x1fsystem-suspend\n"
+    echo -en "Reboot\0icon\x1fsystem-restart\n"
+else
+    if [ "$1" = "Shutdown" ]; then
+        systemctl poweroff
+    elif [ "$1" = "Logout" ]; then
+        bspc quit
+    elif [ "$1" = "Reboot" ]; then
+        systemctl reboot
+    elif [ "$1" = "Suspend" ]; then
+        systemctl suspend
+    fi
+fi

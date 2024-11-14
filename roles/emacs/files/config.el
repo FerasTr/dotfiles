2e4b6e33ffa4d5509621f33f0b22727a62d4dac0
FerasTr <{ID}+{username}@users.noreply.github.com>
2024-11-14 17:31:40 +0200

    wip

--- roles/emacs/files/config.el
+++ roles/emacs/files/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'catppuccin)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -84,5 +84,3 @@
 
 (setq evil-split-window-below t
       evil-vsplit-window-right t)
-
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))

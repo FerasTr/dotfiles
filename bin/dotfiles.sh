f2f4fd8332422172db71d0bec9952989475270cc
FerasTr <32588570+FerasTr@users.noreply.github.com>
2024-12-01 16:11:07 +0200

    additional script fixes.

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -37,52 +37,61 @@ function _cmd {
   exit 1
 }
 
-function _clear_task {
-  TASK=""
+function _update_pkgs {
+  _task "upgrading packages"
+  _cmd "sudo pacman -Syu --noconfirm"
 }
 
-function _task_done {
-  echo "[✓] $TASK"
-  _clear_task
-}
-
-function arch_setup() {
+function _install_ansible {
   if ! [ -x "$(command -v ansible)" ]; then
     _task "Installing Ansible"
-    _cmd "sudo pacman -Sy --noconfirm"
     _cmd "sudo pacman -S --noconfirm ansible"
-    _cmd "sudo pacman -S --noconfirm python-argcomplete"
-    # _cmd "sudo activate-global-python-argcomplete3"
   fi
-  if ! pacman -Q python3 >/dev/null 2>&1; then
-    _task "Installing Python3"
-    _cmd "sudo pacman -S --noconfirm python3"
+}
+
+function _install_python_pkgs {
+  if ! pacman -Q python >/dev/null 2>&1; then
+    _task "Installing Python"
+    _cmd "sudo pacman -S --noconfirm python"
   fi
   if ! pacman -Q python-pip >/dev/null 2>&1; then
-    _task "Installing Python3 Pip"
+    _task "Installing Pip"
     _cmd "sudo pacman -S --noconfirm python-pip"
   fi
-  if ! pip3 list | grep watchdog >/dev/null 2>&1; then
+  if ! pip list | grep watchdog >/dev/null 2>&1; then
     _task "Installing Python3 Watchdog"
     _cmd "sudo pacman -S --noconfirm python-watchdog"
   fi
 
-  if ! pacman -Q openssh >/dev/null 2>&1; then
-    _task "Installing OpenSSH"
-    _cmd "sudo pacman -S --noconfirm openssh"
+  if ! pip list | grep argcomplete >/dev/null 2>&1; then
+    _task "Installing Python argcomplete"
+    _cmd "sudo pacman -S --noconfirm python-argcomplete"
   fi
-
-  _task "Setting Locale"
-  _cmd "sudo localectl set-locale LANG=en_US.UTF-8"
-
-  _task "Installing yay"
-  _cmd "sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && pushd /tmp/yay-bin && makepkg -si && popd"
-
-  _task "Setting up yay"
-  _cmd "yay -Y --gendb"
 }
 
-update_ansible_galaxy() {
+function _install_yay {
+  _task "installing yay"
+  if ! command -v yay >/dev/null; then
+    tmp_dir=$(mktemp -d)
+    function finish { rm -rf "$tmp_dir"; } # clean up after yourself...
+    trap finish EXIT                       # ...no matter how you exist
+
+    git clone https://aur.archlinux.org/yay.git "$tmp_dir"
+    pushd "$tmp_dir"
+    makepkg -sri --noconfirm --needed
+    popd
+
+    if ! command -v yay >/dev/null; then
+      >&2 echo "yay failed to install"
+      exit 1
+    else
+      _task "Setting up yay"
+      _cmd "yay -Y --gendb --noconfirm"
+    fi
+  fi
+}
+
+function update_ansible_galaxy() {
   local os=$1
   local os_requirements=""
   _task "Updating Ansible Galaxy"
@@ -93,14 +102,46 @@ update_ansible_galaxy() {
   _cmd "ansible-galaxy install -r $DOTFILES_DIR/requirements/common.yml $os_requirements"
 }
 
-_cleanup() {
+function run_playbook() {
+  _task "Running playbook"
+  if [[ -f $VAULT_SECRET ]]; then
+    ansible-playbook --vault-password-file $VAULT_SECRET "$DOTFILES_DIR/main.yml" "$@"
+  else
+    ansible-playbook "$DOTFILES_DIR/main.yml" "$@"
+  fi
+}
+
+function pull_dotfiles_repo {
+  # TODO: implement a task that checks for updates locally then pushes to remote
+  if ! [[ -d "$DOTFILES_DIR" ]]; then
+    _task "Cloning repository"
+    _cmd "git clone --quiet https://github.com/FerasTr/dotfiles.git $DOTFILES_DIR"
+  else
+    _task "Updating repository"
+    _cmd "git -C $DOTFILES_DIR pull --quiet"
+  fi
+}
+
+function _cleanup() {
   _task "Remove logs file"
   _cmd "rm $DOTFILES_LOG"
 }
 
-_install() {
+function arch_setup() {
+
+  _update_packages
+
+  _install_ansible
+
+  _install_python_pkgs
+
+  _install_yay
+}
+
+function _install() {
   source /etc/os-release
   _task "Loading Setup for detected OS: $ID"
+
   case $ID in
   arch)
     arch_setup
@@ -111,39 +152,19 @@ _install() {
     ;;
   esac
 
-  # TODO: generate keys to use with ssh and git
-  # if ! [[ -f "$SSH_DIR/authorized_keys" ]]; then
-  #   _task "Generating SSH keys"
-  #   _cmd "mkdir -p $SSH_DIR"
-  #   _cmd "chmod 700 $SSH_DIR"
-  #   _cmd "ssh-keygen -b 4096 -t rsa -f $SSH_DIR/id_rsa -N '' -C $USER@$HOSTNAME"
-  #   _cmd "cat $SSH_DIR/id_rsa.pub >> $SSH_DIR/authorized_keys"
-  # fi
-
-  # TODO: implement a task that checks for updates locally then pushes to remote
-  if ! [[ -d "$DOTFILES_DIR" ]]; then
-    _task "Cloning repository"
-    _cmd "git clone --quiet https://github.com/FerasTr/dotfiles.git $DOTFILES_DIR"
-  else
-    _task "Updating repository"
-    _cmd "git -C $DOTFILES_DIR pull --quiet"
-  fi
+  pull_dotfiles_repo
 
   pushd "$DOTFILES_DIR" 2>&1 >/dev/null
+
   update_ansible_galaxy $ID
 
-  _task "Running playbook"
-  _task_done
-  if [[ -f $VAULT_SECRET ]]; then
-    ansible-playbook --vault-password-file $VAULT_SECRET "$DOTFILES_DIR/main.yml" "$@"
-  else
-    ansible-playbook "$DOTFILES_DIR/main.yml" "$@"
-  fi
+  run_playbook $@
 
   popd 2>&1 >/dev/null
+
 }
 
-main() {
+function main() {
   if [ $# -eq 0 ]; then
     echo "No arguments provided. Usage: dotfiles.sh [command] [options]"
     exit 1
